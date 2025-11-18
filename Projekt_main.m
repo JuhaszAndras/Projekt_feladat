fca5ba710f71953b6cf04defcedcb3c14014438a
JuhaszAndras <84314696+JuhaszAndras@users.noreply.github.com>
2025-11-18 21:52:51 +0100

    Konzi előtti feltöltés

--- Projekt_main.m
+++ Projekt_main.m
@@ -4,9 +4,9 @@ g=9.81;
 m=1;
 M=2;
 l=1;
-s1=-0.5+1i;
+s1=-2+1i;
 s2=conj(s1);
-s_cinf=-5;
+s_cinf=-10;
 
 poles=[s1, s2, s_cinf, s_cinf];
 
@@ -25,4 +25,4 @@ D = zeros(4,1); % rendszerint nulla
 
 sys = ss(A,B,C,D); 
 
-K = acker(A, B, poles);
+K = acker(A, B, poles)
