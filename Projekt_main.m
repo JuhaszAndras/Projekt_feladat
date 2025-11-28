74533cd1fa95aa7665f40c7f584822580439c716
JuhaszAndras <84314696+JuhaszAndras@users.noreply.github.com>
2025-11-28 18:15:52 +0100

    Elakadtam a swing up control résznél

--- Projekt_main.m
+++ Projekt_main.m
@@ -25,6 +25,18 @@ C = eye(4);     % vagy amit szeretnél mérni
 D = zeros(4,1); % rendszerint nulla
 
 
+q11=10;
+q22=1000;
+q33=1;
+q44=500;
+
+Q=[q11,0,0,0
+    0,q22,0,0
+    0,0,q33,0
+    0,0,0,q44];
+R=1;
+
 sys = ss(A,B,C,D); 
 
-K = acker(A, B, poles)
+%K = acker(A, B, poles);
+K=lqr(A,B,Q,R)
